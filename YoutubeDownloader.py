7bee8664a41d05a79392920a6d4540c1b4496d06
Abdulakeem Asiru <97950441+Akaywonder1124@users.noreply.github.com>
2022-04-27 20:03:29 -0700

    first commit

---
+++ YoutubeDownloader.py
@@ -0,0 +1,69 @@
+from distutils.log import ERROR
+from urllib.error import URLError
+from pytube import *
+from tkinter import *
+import youtube_dl
+
+
+#layout and title
+root = Tk()
+root.geometry('500x300')
+root.resizable(0,0)
+root.title("Youtube Downloader")
+root.configure(bg="black")
+
+
+#label
+Label(root, text = "A SIMPLE YOUTUBE DOWNLOADER ", font= "roboto 10 bold",
+     fg= "orange", bg= "black", pady= 12, bd =1 ).pack()
+link = StringVar()
+
+
+Label(root, text = "PASTE THE URL OF THE YOUTUBE VIDEO ", font= "roboto 10",
+     fg= "orange", bg= "black", pady= 12, bd =1 ).pack()
+
+#Download image
+photo = PhotoImage(file= r"C:\Users\USER\Downloads\downloadbt.png") 
+
+
+#Input Field
+link_enter = Entry(root, width= "70",  textvariable= link ).place(x = 30, y=85)
+
+
+
+def download():
+     try:
+          url = {'outtmpl': 'C:/Users/USER/Downloads/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s'}
+          with youtube_dl.YoutubeDL(url) as video:
+               video.download([str(link.get())])
+          # Display a successful message
+          url = YouTube(str(link.get())) #Get url form the link input declared earlier
+          #Displays Youtube title
+          url.thumbnail_url
+          if download == True:
+               Label(root, text = "Downlaoding.....", font= "roboto 18 bold",fg= "green",
+                bg= "black", pady= 12, bd =1 ).place(y=200, x = 80)
+
+     except (URLError, youtube_dl.utils.DownloadError):
+            Label(root, text = "Internet Connection Error", font= "roboto 15 bold",fg= "red",
+                bg= "black", pady= 12, padx= 30, bd =1 ).place(y=200, x = 80)
+
+     else:
+   
+          Label(root, text = "Successfully Downloaded", font= "roboto 18 bold",fg= "green",
+                bg= "black", pady= 12, bd =1 ).place(y=200, x = 80)
+     
+
+
+   
+     
+
+#Download Button
+Download = Button(root, text= "Download", image= photo, bg="black", bd=0, 
+                    cursor="arrow black", command= download ).place(y = 130, x = 100)
+#Author
+Autor = Label(root, text = "Proudly Developed by Akay ", font= "roboto 10",fg= "orange",
+                bg= "black", pady= 12, bd =2 ).place(y=250, x = 320)
+
+root.mainloop()
+
